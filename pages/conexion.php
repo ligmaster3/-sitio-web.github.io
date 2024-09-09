05da3ec48bdc05e66ce05f28a0a8def6c6119013
ligmaster <136215596+ligmaster3@users.noreply.github.com>
2024-09-08 19:31:13 -0500

    Update conexion.php

--- pages/conexion.php
+++ pages/conexion.php
@@ -2,12 +2,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "elecciones";
+$database = "gestion_de_usuario";
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $database);
 // Chequear conexión
 if ($conn->connect_error) {
- echo("Connection failed: " . $conn->connect_error);
+ die("Connection failed: " . $conn->connect_error);
 }
  echo("conexion establecida");
 ?>
