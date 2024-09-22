1b674cdb926232b398bd5a3f179de3ce4a3fa78a
ligmaster <136215596+ligmaster3@users.noreply.github.com>
2024-09-22 15:49:59 -0500

    Update signup.php

--- pages/view/signup.php
+++ pages/view/signup.php
@@ -14,8 +14,14 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <!-- Custom styles for our template -->
+    <link rel="stylesheet" href="../css/style-sesion.css">
+
     <!-- Article main content -->
     <article class="col-xs-12 maincontent">
+        <header class="page-header">
+            <h1 class="page-title">Registro</h1>
+        </header>
 
         <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 container">
             <div class="panel panel-default">
